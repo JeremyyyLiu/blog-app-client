4555285e32ab03ef5a9e40133e273e922a6920ef
Jeremy Liu <unswmercury@gmail.com>
2023-06-07 09:56:21 +1000

    Add utils folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "./utils/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
