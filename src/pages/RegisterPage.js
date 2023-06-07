6b57776649687f566cb697ba72799d5ac0eeec66
Jeremy Liu <unswmercury@gmail.com>
2023-06-07 11:20:14 +1000

    Add config.js and .env

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import serverUrl from "../utils/config";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Handle register function
+  function handleRegister(event) {
+    event.preventDefault();
+  }
+
   return (
     <form className="register">
+      {/* Header */}
       <h1>Register</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+
+      {/* Input field */}
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      {/* Submit button */}
       <button>Login</button>
     </form>
   );
