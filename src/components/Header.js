3e911d82616ae403c5ed7b0545a728cb7a58589b
Jeremy Liu <unswmercury@gmail.com>
2023-06-08 17:51:11 +1000

    Add logout function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,52 @@
 import "../App.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [username, setUsername] = useState(null);
+
+  // Check if the user is logged in
+  useEffect(() => {
+    fetch("http://localhost:8080/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  // Request for logout
+  function logout() {
+    fetch("http://localhost:8080/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+  }
+
   return (
     <header>
       <Link to="/" className="logo">
         BlogApp
       </Link>
 
+      {/* If user logged in */}
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+
+        {/* If user does not login */}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
